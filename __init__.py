2b8af545f2d585b0f1bf66cdb374b284427cfff0
izzetcan <izzetcankarakus@gmail.com>
2024-05-31 00:07:35 +0300

    changes
    Former-commit-id: f3f6f80cf7db9fabc7e3ba8579d351a7a4259b4b

--- __init__.py
+++ __init__.py
@@ -2,6 +2,20 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-__version__ = '0.0.1'
-__author__ = 'REMOVED_CARDHOLDER_NAME'
-__license__ = 'MIT'
+default_settings = {
+    "log_path": "bot_data/logs/",
+}
+
+__version__ = "0.0.1"
+__author__ = "REMOVED_CARDHOLDER_NAME"
+__license__ = "MIT"
+
+
+def configure():
+    """Configure the bot with the given settings."""
+    default_settings.update(default_settings)
+    globals().update(default_settings)
+
+
+if __name__ == "__main__":
+    configure()
